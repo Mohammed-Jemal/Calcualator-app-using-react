3bb620420d866dfe9f6836aade13819dda377971
neway gebrie <niwaygebrie@gmail.com>
2023-04-13 10:06:33 -0400

    allc ahnges made

---
+++ src/DigitButton.js
@@ -0,0 +1,16 @@
+import { ACTIONS } from "./App"
+import React from 'react'
+
+function DigitButton({dispatch,digit}) {
+  return (
+    <button
+      onClick={() => dispatch({ 
+        type: ACTIONS.ADD_DIGIT,
+       payload: { digit } })}
+  >
+      {digit}
+    </button>
+  )
+}
+
+export default DigitButton
